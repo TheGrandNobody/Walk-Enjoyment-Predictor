73c91d22497210d89e23bcf8a1b6848a7aaa5810
MerelFlorian <merelflorian146@gmail.com>
2023-05-22 02:31:25 +0200

    boxplot.py

---
+++ boxplot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('merged_data_upsample.csv')
+# Calculate the average of each of the last six columns
+df['average'] = df.iloc[:, -6:].mean(axis=1)
+
+# Create a boxplot of these averages
+plt.figure(figsize=(12, 8))
+plt.boxplot(df['average'])
+plt.title('Boxplot of Averaged PACE values')
+plt.xlabel('Average PACE score')
+plt.show()
